b080974b532cb41b09ccd25c8023f31d58f7f968
Ayush Katre <ayushkatre1801@gmail.com>
2025-04-21 11:01:13 +0530

    postman testing done using apis

---
+++ src/app/api/file/[projectId]/[fileName]/route.ts
@@ -0,0 +1,78 @@
+import { NextResponse, NextRequest } from "next/server";
+import { connectDB } from "@/config/connectDB";
+import FileModel from "@/models/FileModel";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ projectId: string; fileName: string }> }
+) {
+  const { projectId, fileName } = await params;
+
+  if (!projectId || !fileName) {
+    return new NextResponse("Provided projectId and fileName", {
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  }
+
+  //get extension of fileName
+  const extArray = fileName?.toString()?.split(".");
+  const extension = extArray[extArray?.length - 1];
+
+  try {
+    await connectDB();
+
+    const getFile = await FileModel.findOne({
+      name: fileName,
+      projectId: projectId,
+    });
+
+    const content = getFile.content
+
+    if (extension === "html") {
+    
+      const host = request.headers.get("host"); //domain
+      const protocol = host?.includes("localhost") ? "http" : "https";
+      const DOMAIN = `${protocol}://${host}`;
+
+      const URL = `${DOMAIN}/api/file/${projectId}`
+      const replaceHTML = content.replace(/(src|href)=["']@(.*?)["']/g,`$1=${URL}$2`);
+
+      return new NextResponse(replaceHTML, {
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    }
+    else if(extension === 'css'){
+        return new NextResponse(content, {
+            headers: {
+              "content-type": "text/css",
+            },
+          }); 
+    }
+    else if(extension === 'js'){
+        return new NextResponse(content, {
+            headers: {
+              "content-type": "text/javascript",
+            },
+          }); 
+    }
+
+
+    return new NextResponse(content, {
+        headers: {
+          "content-type": "text/text",
+        },
+    }); 
+
+
+  } catch (error) {
+    return new NextResponse("Something went wrong", {
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  }
+}
